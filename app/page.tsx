a40fad5128b88bb841f2d06d5547bb7f83e4f9fb
kupre1 <160654812+kupre1@users.noreply.github.com>
2024-11-15 23:59:22 +0400

    last change

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Icon, Icon1, Icon2, starts, starts1 } from "@/potos";
+import { Icon, Icon1, Icon2, starts, starts1, Icon3, Icon4 } from "@/potos";
 import Image from "next/image";
 
 export default function Home() {
@@ -58,9 +58,55 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div className="min-w-[419px]  flex flex-col justify-between gap-[10px]">
+        <div className="text-[#999999] flex items-center gap-[4px] ">
+          <Image src={Icon3} width={24} height={24} alt="icon" />
+          <p className="text-[18px] font-medium leading-7">Gernes</p>
+        </div>
+        <div className="flex  gap-[10px]">
+          <div className="bg-black border border-gray-800 w-[101px] h-[43px] text-white text-[18px] leading-7 flex justify-center items-center rounded-md">
+            Sci-Fi TV
+          </div>
+          <div className="bg-black border border-gray-800 w-[155px] h-[43px] text-white text-[18px] leading-7 flex justify-center items-center rounded-md">
+            Teen TV Shows
+          </div>
+          <div className="bg-black border border-gray-800 w-[137px] h-[43px] text-white text-[18px] leading-7 flex justify-center items-center rounded-md">
+            US TV Shows
+          </div>
+        </div>
+      </div>
+      <div className="min-w-[419px] gap-5 flex flex-col justify-between ">
+        <div className="text-[#999999] flex items-center gap-[4px] ">
+          <p className="text-[18px] font-medium leading-7">Director</p>
+        </div>
+        <div className="min-w-[419px]  flex justify-between gap-[10px]">
+          <div className="w-[419px] h-[96px] bg-black border border-gray-800 py-4 pl-4 rounded-md flex gap-2  ">
+            <Image src={Icon4} width={56} height={60} alt="icon" />
+            <div className="flex flex-col justify-center ">
+              <p className="font-medium leading-6 text-[18px] text-white">
+                The Duffer Brothers
+              </p>
+              <p className="font-medium leading-6 text-[#999999]">From USA</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="min-w-[419px] gap-5 flex flex-col   justify-between ">
+        <div className="text-[#999999] flex items-center gap-[4px] ">
+          <p className="text-[18px] font-medium leading-7">Director</p>
+        </div>
+        <div className="min-w-[419px]  flex justify-between gap-[10px]">
+          <div className="w-[419px] h-[96px] bg-black border border-gray-800 py-4 pl-4 rounded-md flex gap-2  ">
+            <Image src={Icon4} width={56} height={60} alt="icon" />
+            <div className="flex flex-col justify-center ">
+              <p className="font-medium leading-6 text-[18px] text-white">
+                The Duffer Brothers
+              </p>
+              <p className="font-medium leading-6 text-[#999999]">From USA</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- potos/index.ts
+++ potos/index.ts
@@ -1,7 +1,9 @@
 import Icon from "./Icon.png";
 import Icon1 from "./Icon1.png";
 import Icon2 from "./Icon2.png";
+import Icon3 from "./Icon3.png";
+import Icon4 from "./Image4.png";
 import starts from "./SubContainer1.png";
 import starts1 from "./Sub Container.png";
 
-export { Icon, Icon1, Icon2, starts, starts1 };
+export { Icon, Icon1, Icon3, Icon4, Icon2, starts, starts1 };
