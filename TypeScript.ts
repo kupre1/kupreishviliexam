207c050bed25c2b7b07b019621a2b2c7ae81ce25
kupre1 <160654812+kupre1@users.noreply.github.com>
2024-11-15 20:56:03 +0400

    comit

--- TypeScript.ts
+++ TypeScript.ts
@@ -2,33 +2,42 @@
 
 // მაგალითი 1
 
-let name = "Alice";
+let name1: string = "Alice";
 
 // მაგალითი 2
 
-let numbers = [1, 2, 3];
+let numbers: (number | number | number)[] = [1, 2, 3];
 
 // მაგალითი 3
 
-function multiply(a, b) {
+function multiply(a: number, b: number): number {
   return a * b;
 }
 
 // მაგალითი 4
 
-const user = { id: 1, name: "Alice" };
+const user1: { id: number; name: string } = { id: 1, name: "Alice" };
 
 // მაგალითი 5
 // რისთვის ვიყენებთ არსებული კოდში = Guest-ს :
 
 function greet(name: string = "Guest") {}
 
-// პასუხი:
+// პასუხი: როდესაც ფუნქცია greets გამოვიყენებთ და ნამეი არ იქნება გამოყენებული სულ მიიღებს guests
 
 // მაგალითი 6
 // დაწერეთ ტიპი რომელიც მოერგება ყველა ქვემოთ ჩამოთვლილ ელემენტს. მაგ: type Config = {
 
-const config1 = {
+type Config = {
+  tester?: string;
+  theme?: string;
+  options?: {
+    fontSize: number;
+    layout: null;
+  };
+};
+
+const config1: Config = {
   theme: "dark",
   options: {
     fontSize: 16,
@@ -36,35 +45,63 @@ const config1 = {
   },
 };
 
-const config2 = {
+const config2: Config = {
   theme: "dark",
 };
 
-const config3 = {
+const config3: Config = {
   tester: "test",
 };
 
 // მაგალითი 7
 // დაწერეთ ტიპი რომელიც მოერგება ყველა ქვემოთ ჩამოთვლილ ელემენტს. მაგ: type MixedArray =
-const array1 = [42, "hello", { name: "Alice" }];
-const array2 = ["apple", true, { isValid: false }];
-const array3 = [];
+
+type MixedArray = (
+  | number
+  | string
+  | { name: string }
+  | boolean
+  | { isValid: boolean }
+)[];
+
+const array1: MixedArray = [42, "hello", { name: "Alice" }];
+const array2: MixedArray = ["apple", true, { isValid: false }];
+const array3: MixedArray = [];
 
 // მაგალითი 8
 // აღწერეთ რისი ტიპიზაცია ხდება არსებულ კოდში წერილობით
 
 type Handler = {
   process?: () => string;
+  //არის ფუნქცია, რომელსაც არ აქვს არგუმენტები, მაგრამ აბრუნებს string ტიპს
   validate?: (input: string) => boolean;
+  // validate ფუნქცია, რომელიც მიიღებს string ტიპის არგუმენტს და დააბრუნებს boolean-ს
   log?: () => void;
+  // log ფუნქცია, რომელიც არ აბრუნებს არაფერს
 };
 
 // მაგალითი 9
 
 // შექმენით ტიპი მონაცემისთივს:
 
-type User = {};
-
+type User = {
+  id: number;
+  username: string;
+  isAdmin: boolean;
+  profile: {
+    fullName: string;
+    age: number;
+    interests: string[];
+  };
+  settings: {
+    theme: string;
+    notifications: {
+      email: boolean;
+      sms: boolean;
+    };
+  };
+  metadata: undefined;
+};
 const user: User = {
   id: 101,
   username: "tech_learner",
